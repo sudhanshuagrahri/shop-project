a90f1fe0bb78737e88d824fc955b4e0063ba8a83
sudhanshuagrahri <71632862+sudhanshuagrahri@users.noreply.github.com>
2021-04-09 21:41:58 +0530

    Add files via upload
    done by Sudhanshu

---
+++ shop/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'available', 'quantity', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available', 'quantity']
+    prepopulated_fields = {'slug': ('name',)}
+
